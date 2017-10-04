1a87801874930635d769dc23b70651d94f7119cc
QTSEL\VMA <vicente.marrero@qlik.com>
2017-10-04 19:15:59 +0100

    Add geolocation support via geolocation api

--- server/utils/message.js
+++ server/utils/message.js
@@ -2,8 +2,16 @@ var generateMessage = (from, text) => {
     return {
         from,
         text,
-        createdAt: new Date().getTime()
+        createdAt: Date.now()
     };
 };
 
-module.exports = {generateMessage};
+var generateLocationMessage = (from, latitude, longitude) => {
+    return {
+        from,
+        url: `https://www.google.com/maps?q=${latitude},${longitude}`,
+        createdAt: Date.now()
+    };
+};
+
+module.exports = {generateMessage, generateLocationMessage};
