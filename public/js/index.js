d0095a74f92898e826ab85004da58bae5069d5c6
QTSEL\VMA <vicente.marrero@qlik.com>
2017-10-06 19:39:38 +0100

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -28,11 +28,12 @@ socket.on('newLocationMessage', function(message) {
 
 jQuery('#message-form').on('submit', function(event) {
     event.preventDefault();
+    var messageTextBox = jQuery('[name=message]');
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextBox.val()
     }, function() {
-        console.log('Got message');
+        messageTextBox.val('');
     });
 });
 
@@ -42,12 +43,17 @@ locationButton.on('click', function() {
     if(!navigator.geolocation) {
         return alert('Geolocation not supported by your browser');
     }
+
+    locationButton.attr('disabled', 'disabled').text('Sending Location...');
+
     navigator.geolocation.getCurrentPosition(function(position) {
+        locationButton.removeAttr('disabled').text('Send Location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function() {
-        alert('Unable to fetch location');
+        locationButton.removeAttr('disabled');
+        alert('Unable to fetch location').text('Send Location');
     });
 });
